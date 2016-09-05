9f2e5ab858df79d5222d15817720fdda906f943b
BP-akumar168 <akumar18@xavient.com>
2016-09-05 14:22:03 +0530

    initial commit

---
+++ src/main/java/com/customer/management/tool/CustomerManagementToolApplication.java
@@ -0,0 +1,37 @@
+package com.customer.management.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+@EnableWebMvc
+public class CustomerManagementToolApplication extends WebMvcConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagementToolApplication.class, args);
+	}
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	 @Bean
+	    public InternalResourceViewResolver viewResolver() {
+	        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	        resolver.setPrefix("WEB-INF/ui/");
+	        resolver.setSuffix(".jsp");
+	        return resolver;
+	    }
+	 
+	/* @Override
+     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+             registry.addResourceHandler("/resources/**")
+                     .addResourceLocations("/resources/");
+     }*/
+}
---
+++ src/main/webapp/WEB-INF/ui/login.jsp
@@ -0,0 +1,31 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+
+
+     
+        <title>Customer Management Tool</title>
+        <link href="../../css/loginstyle.css" rel="stylesheet" type="text/css" />
+
+    </head>
+<body>
+<form>
+  <div align="center"><header >Customer Management Tool</header></div>
+  <label>Username <span>*</span></label>
+  <input/>
+  <div class="help">At least 6 character</div>
+  <label>Password <span>*</span></label>
+  <input/>
+  <div class="help">Use upper and lowercase lettes as well</div>
+  
+    <label>Roll <span>*</span></label>
+    
+  <button>Login</button>
+</form>
+    
+
+
+    </body>
+</html>
